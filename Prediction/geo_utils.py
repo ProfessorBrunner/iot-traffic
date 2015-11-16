a2a91a289775b9465bd2fe4286c814550a337197
Neeraj <neeasthana@gmail.com>
2015-11-16 13:34:11 -0600

    hazard additions

--- Prediction/geo_utils.py
+++ Prediction/geo_utils.py
@@ -6,11 +6,12 @@ from geopy.distance import VincentyDistance
 
 
 class Point:
-    def __init__(self, latitude, longitude, timestamp, car_id):
+    def __init__(self, latitude, longitude, timestamp, car_id, hazard=None):
         self.latitude = latitude
         self.longitude = longitude
         self.timestamp = timestamp
         self.car_id = car_id
+        self.hazard = hazard
 
 
 """Convert String timestamp in file to time variable
@@ -52,9 +53,9 @@ def calculate_bearing(locationOne, locationTwo):
 
 
 def next_location(origin, speed, interval, bearing):
-    print("The speed of the vehicle is: " + str(speed) + " km/s")
+    #print("The speed of the vehicle is: " + str(speed) + " km/s")
     d = speed*interval
-    print("In " + str(interval) + " seconds the vehicle will travel " + str(d) + " km")
+    #print("In " + str(interval) + " seconds the vehicle will travel " + str(d) + " km")
     destination = VincentyDistance(kilometers=d).destination(origin, bearing)
     return (destination.latitude, destination.longitude)
 
@@ -66,9 +67,9 @@ in order to predict the vehicles next Location
 """
 def predict_next_location(origin, current, time, interval, steps=1):
     distance = calculate_distance(origin, current)
-    print("The distance between the two points is: " + str(distance) + " km")
+    #print("The distance between the two points is: " + str(distance) + " km")
     bearing = calculate_bearing(origin, current)
-    print("The bearing of the two points is: " + str(bearing) + " degrees")
+    #print("The bearing of the two points is: " + str(bearing) + " degrees")
     for step in range(steps):
         current = next_location(current, speed(distance, time), interval, bearing)
     return current
--- Prediction/speed_drop.py
+++ Prediction/speed_drop.py
@@ -3,30 +3,53 @@ import time
 import geo_utils as geo
 import graphs
 
-with open('../SyntheticData/src/data_for_sudden_dip.json') as json_data:
-    data = json.load(json_data)[0]
-    json_data.close()
 
+def predictions(points, hazards = [], hazard_decrease = .5):
+    count = 0
     prev_point = None
 
     speeds = []
+    next_locations = []
+    for point in points:
+        if prev_point != None:
+            l1 = (point.latitude, point.longitude)
+            l2 = (prev_point.latitude, prev_point.longitude)
+
+            distance = geo.calculate_distance(l1,l2)
+            time = point.timestamp - prev_point.timestamp
+            speed = geo.speed(distance, time)
+
+            predict = geo.predict_next_location(l2, l1, time, time)
+            error = geo.error(l2,predict)
+
+            speeds.append(speed * 1000)
+            next_locations.append(predict)
+            print(error)
+        prev_point = point
+        count += 1
+
+    return speeds, next_locations
+
+if __name__ == '__main__':
+    json_data = open('../SyntheticData/src/data_for_sudden_dip.json')
+    data = json.load(json_data)[0]
+    json_data.close()
+
+    #add all points to an array
+    points = []
     for point in data:
         current = geo.Point(point['latitude'], \
             point['longitude'], \
             geo.convert_timestamp(point['time_stamp']), \
             point['car_id'] \
         )
+        points.append(current)
 
-        if prev_point != None:
-            distance = geo.calculate_distance((current.latitude, current.longitude),(prev_point.latitude, prev_point.longitude))
-            time = current.timestamp - prev_point.timestamp
-            speed = geo.speed(distance, time)
+    #create road hazard
+    road_hazard = [(250,500)]
+    hazard_decrease = .5
 
-            speeds.append(speed * 1000)
 
-            print(distance)
-        prev_point = current
+    speeds, next_locations = predictions(points, road_hazard, hazard_decrease)
 
     graphs.graph_speeds(speeds, 'm/s')
-
-
