9046a5c751ade3a1f6cce64d66fe7e9c15d4cd25
Neeraj Asthana <neeasthana@gmail.com>
2016-06-09 20:11:01 +0000

    final notebook commit

--- Prediction/notebooks/timedecaykafka.py
+++ Prediction/notebooks/timedecaykafka.py
@@ -1,5 +1,6 @@
 
-##Spark Kafka
+from __future__ import print_function
+
 import pyspark
 from pyspark import SparkContext, SparkConf
 from pyspark.streaming import StreamingContext
@@ -9,12 +10,21 @@ from pyspark.streaming.kafka import KafkaUtils
 conf = SparkConf().setAppName("Kafka-Spark")
 sc = SparkContext(conf=conf)
 
-# create StreamingContext (updates every 30 seconds)
+# create StreamingContext
 ssc = StreamingContext(sc, 30)
 
+# new approach (w/o receivers)
 topic = ["mytopic"]
 brokers =  "141.142.236.172:9092,141.142.236.194:9092"
 directKafkaStream = KafkaUtils.createDirectStream(ssc, topic, {"metadata.broker.list": brokers})
 
+lines = directKafkaStream.map(lambda x: x[1])
+counts = lines.flatMap(lambda line: line.split(" ")) \
+        .map(lambda word: (word, 1)) \
+        .reduceByKey(lambda a, b: a+b)
+counts.pprint()
+
 ssc.start()
 ssc.awaitTermination()
+
+print("finished this")
